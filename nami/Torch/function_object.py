9ad3b9843151bebc9f3f213eabb95bfa59d6343b
kandarpa02 <kandarpaexe@gmail.com>
2025-06-15 14:54:10 +0530

    removed torch.jit.script from nami functional

--- nami/Torch/function_object.py
+++ nami/Torch/function_object.py
@@ -6,46 +6,7 @@ try:
 except:
     raise ImportError("PyTorch is not installed")
 
-
-class nami_forward_backward(torch.autograd.Function):
-    @staticmethod
-    def forward(ctx, x, w, a, b):
-        out = nami(_x=x, w=w, a=a, b=b)
-        ctx.save_for_backward(out, x, w, a, b)
-        return out
-    
-    @staticmethod
-    def backward(ctx, grad_output):
-        out, x, w, a, b = ctx.saved_tensors
-
-        grad_input = grad_output * torch.where(
-            x > 0,
-            a * (1 - torch.tanh(x * a) ** 2),
-            a * w * torch.cos(x * w) / b
-        )
-
-        grad_w = grad_output * torch.where(
-            x > 0,
-            torch.zeros_like(x),
-            a * x * torch.cos(x * w) / b
-        ).sum()
-
-        grad_a = grad_output * torch.where(
-            x > 0,
-            x * (1 - torch.tanh(x * a) ** 2),
-            torch.sin(x * w) / b
-        ).sum()
-
-        grad_b = grad_output * torch.where(
-            x > 0,
-            torch.zeros_like(x),
-            -a * torch.sin(x * w) / (b ** 2)
-        ).sum()
-
-        return grad_input, grad_w, grad_a, grad_b
-
         
-
 class Nami(nn.Module):
     '''
     PyTorch implementation of Nami
@@ -78,4 +39,4 @@ class Nami(nn.Module):
 
 
     def forward(self, x):
-        return nami_forward_backward.apply(x, self.w, self.a, self.b)
+        return nami(_x=x, w=self.w, a=self.a, b=self.b)
